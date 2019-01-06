92a4a36dc56e13c1962a19c88041e95e3f6ab4de
RyoKojima <ryokojima@kojima.com>
2019-01-06 09:05:35 +0900

    update to openzeppelin-solidity@v2.1.1

--- mint/contracts/SimpleToken.sol
+++ mint/contracts/SimpleToken.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
@@ -8,8 +8,8 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 // 2.ERC20Mintable.solを継承
 contract SimpleToken is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
-        string name,
-        string symbol,
+        string memory name,
+        string memory symbol,
         uint8 decimals,
         uint256 initSupply
     )
